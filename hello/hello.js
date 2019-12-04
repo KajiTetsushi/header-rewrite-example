0c42cb3401bf7905e18a5074c947695e47cabead
Kaji Tetsushi <kajitetsushi@outlook.com>
2019-12-04 10:16:54 +0000

    feat(hello): demonstrate link won't be overwritten outside of app

--- hello/hello.js
+++ hello/hello.js
@@ -5,8 +5,8 @@ const port = 3030;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!\n');
+  res.setHeader('Content-Type', 'text/html');
+  res.end('Hello World! <a href="/">Link to itself</a>');
 });
 
 server.listen(port, hostname, () => {
